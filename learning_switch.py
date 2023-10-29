09a122137beef1dbbf5342bff40ae488b50f166d
ranjulbn20 <ranjulbn20@gmail.com>
2023-10-29 14:02:59 +0530

    Change done

--- learning_switch.py
+++ learning_switch.py
@@ -3,6 +3,14 @@ from mininet.net import Mininet
 from mininet.node import RemoteController
 from mininet.cli import CLI
 
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.ofproto import ofproto_v1_3
+from ryu.ofproto.ofproto_v1_3_parser import OFPInstructionActions
+from ryu.ofproto.ofproto_v1_3_parser import OFPActionOutput
+
 class CustomTopology(Topo):
     def build(self):
         # Add switches
@@ -23,6 +31,29 @@ class CustomTopology(Topo):
         self.addLink(h3, s1)
         self.addLink(h4, s2)
         self.addLink(h5, s2)
+
+class CustomRyuApp(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+
+    def __init__(self, *args, **kwargs):
+        super(CustomRyuApp, self).__init__(*args, **kwargs)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        actions = [OFPActionOutput(ofproto.OFPP_FLOOD)]
+        inst = [OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS, actions)]
+
+        out = parser.OFPPacketOut(
+            datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
+            actions=actions, data=msg.data, instructions=inst
+        )
+        datapath.send_msg(out)
 
 def create_topology():
     topo = CustomTopology()
