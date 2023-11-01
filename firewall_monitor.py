7dd148063ac759a0477d16bf22f8377d87050ec2
ranjulbn20 <ranjulbn20@gmail.com>
2023-11-01 17:46:27 +0530

    update

--- firewall_monitor.py
+++ firewall_monitor.py
@@ -10,19 +10,19 @@ class CustomTopology(Topo):
         s2 = self.addSwitch('s2')
 
         # Add hosts
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
-        h3 = self.addHost('h3')
-        h4 = self.addHost('h4')
-        h5 = self.addHost('h5')
+        self.h1 = self.addHost('h1')
+        self.h2 = self.addHost('h2')
+        self.h3 = self.addHost('h3')
+        self.h4 = self.addHost('h4')
+        self.h5 = self.addHost('h5')
 
         # Add links
         self.addLink(s1, s2)
-        self.addLink(h1, s1)
-        self.addLink(h2, s1)
-        self.addLink(h3, s1)
-        self.addLink(h4, s2)
-        self.addLink(h5, s2)
+        self.addLink(self.h1, s1)
+        self.addLink(self.h2, s1)
+        self.addLink(self.h3, s1)
+        self.addLink(self.h4, s2)
+        self.addLink(self.h5, s2)
 
 topos = {'mytopo': (lambda: CustomTopology())}
 
@@ -37,13 +37,13 @@ s1 = net.get('s1')
 s2 = net.get('s2')
 
 # Block communication between H2 and H3 with H5 on switch S1
-s1.dpctl('add-flow in_port=1,dl_dst=' + h2.MAC() + ',actions=drop')
-s1.dpctl('add-flow in_port=1,dl_dst=' + h3.MAC() + ',actions=drop')
-s1.dpctl('add-flow in_port=1,dl_dst=' + h5.MAC() + ',actions=drop')
+s1.dpctl('add-flow in_port=1,dl_dst=' + net.h2.MAC() + ',actions=drop')
+s1.dpctl('add-flow in_port=1,dl_dst=' + net.h3.MAC() + ',actions=drop')
+s1.dpctl('add-flow in_port=1,dl_dst=' + net.h5.MAC() + ',actions=drop')
 
 # Block communication between H1 and H4 on switch S2
-s2.dpctl('add-flow in_port=1,dl_dst=' + h1.MAC() + ',actions=drop')
-s2.dpctl('add-flow in_port=1,dl_dst=' + h4.MAC() + ',actions=drop')
+s2.dpctl('add-flow in_port=1,dl_dst=' + net.h1.MAC() + ',actions=drop')
+s2.dpctl('add-flow in_port=1,dl_dst=' + net.h4.MAC() + ',actions=drop')
 
 # Count packets coming from H3 on switch S1
 s1.dpctl('add-flow in_port=3,actions=controller')
