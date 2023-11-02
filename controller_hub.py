99c43e6a200d091cb60cdd4fbe1bd1b05d75ed17
ranjulbn20 <ranjulbn20@gmail.com>
2023-11-02 12:22:29 +0530

    update

--- controller_hub.py
+++ controller_hub.py
@@ -90,7 +90,7 @@ class SimpleSwitch13(app_manager.RyuApp):
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
-        self.mac_to_port[dpid][src] = in_port
+        #self.mac_to_port[dpid][src] = in_port
 
         # if dst in self.mac_to_port[dpid]:
         #     out_port = self.mac_to_port[dpid][dst]
