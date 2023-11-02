1ae168d51cabd5a36d9bffc49bd5e7a934248222
ranjulbn20 <ranjulbn20@gmail.com>
2023-11-02 11:17:33 +0530

    update

---
+++ learning_switch_mininet.py
@@ -0,0 +1,35 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import RemoteController
+from mininet.cli import CLI
+
+class LearningSwitch(Topo):
+    def build(self):
+        # Add switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+
+        # Add hosts
+        h1 = self.addHost('h1')
+        h2 = self.addHost('h2')
+        h3 = self.addHost('h3')
+        h4 = self.addHost('h4')
+        h5 = self.addHost('h5')
+
+        # Add links
+        self.addLink(s1, s2)
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s2)
+        self.addLink(h5, s2)
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    topo = LearningSwitch()
+    c1 = RemoteController('c1', ip='127.0.0.1')
+    net = Mininet(topo=topo, controller=c1)
+    net.start()
+    #net.pingAll()
+    CLI(net)
+    net.stop()
