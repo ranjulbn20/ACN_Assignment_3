66d7affd9073296b40cbf6dcc57ffbb4ccf6d8de
ranjulbn20 <ranjulbn20@gmail.com>
2023-11-05 12:08:32 +0530

    update switch

--- learning_switch.py
+++ learning_switch.py
@@ -1,18 +1,3 @@
-# Copyright (C) 2011 Nippon Telegraph and Telephone Corporation.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 from ryu.base import app_manager
 from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
@@ -37,13 +22,6 @@ class SimpleSwitch13(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # install table-miss flow entry
-        #
-        # We specify NO BUFFER to max_len of the output action due to
-        # OVS bug. At this moment, if we specify a lesser number, e.g.,
-        # 128, OVS will send Packet-In with invalid buffer_id and
-        # truncated packet data. In that case, we cannot output packets
-        # correctly.  The bug has been fixed in OVS v2.1.0.
         match = parser.OFPMatch()
         actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
                                           ofproto.OFPCML_NO_BUFFER)]
@@ -66,8 +44,7 @@ class SimpleSwitch13(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
+        
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -81,8 +58,8 @@ class SimpleSwitch13(app_manager.RyuApp):
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
-            # ignore lldp packet
             return
+        
         dst = eth.dst
         src = eth.src
 
@@ -91,30 +68,7 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
 
-        # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
-
-        # ip1 = '10.0.0.1'
-        # ip2 = '10.0.0.2'
-        # ip3 = '10.0.0.3'
-        # ip4 = '10.0.0.4'
-        # ip5 = '10.0.0.5'
-
-        # pair_tuple = ((ip1, ip4), (ip4, ip1), (ip2, ip5),
-        #               (ip5, ip2), (ip3, ip5), (ip5, ip3))
-
-        # ip = pkt.get_protocol(ipv4.ipv4)
-
-        # if ip:
-        #     sc = ip.src
-        #     dest = ip.dst
-        #     if (sc, dest) in pair_tuple:
-        #         return
-            
-        # if dpid == 1:
-        #     if in_port == 4:
-        #         self.countPackets +=1
-        #         print("Count of packets from H3 on switch 1: ",self.countPackets)
 
         if dst in self.mac_to_port[dpid]:
             out_port = self.mac_to_port[dpid][dst]
